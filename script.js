6b8406d94be509fba3fdccfa93c4440d16f6efed
aaa <252092@tsm.aou.edu.kw>
2026-03-27 13:44:37 +0300

    Add script.js for interactive functionality of news template

---
+++ script.js
@@ -0,0 +1,18 @@
+// script.js
+
+// Interactive functionality for the news template
+
+// Function to fetch and display news articles
+function fetchNews() {
+    // Your fetch logic here
+}
+
+// Function to display a single article
+function displayArticle(article) {
+    // Your display logic here
+}
+
+// Event listener for loading news on page load
+window.onload = function() {
+    fetchNews();
+};
